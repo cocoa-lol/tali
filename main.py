d3212d0cafb63c3ce1e71eace390d62dc6fc859b
cocoa <171772578+cocoa-lol@users.noreply.github.com>
2025-05-29 21:17:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Test" #return redirect(url_for('/join'))
+
+if __name__ == "__main__":
+    port = 80
+    desc = {'path': '/'}
+    zeroconf.register_service(service)
+    print(f"[+] Up!")
+    try:
+        serve(app, host="0.0.0.0", port=port)
+    finally:
+        exit()
